7c17b4d4de4655cab346c6edbee42a552d11d057
Shunran Sasaki <ssk@sasaki.world>
2020-10-09 03:58:57 +0900

    create: add based of random_hand event

--- main.py
+++ main.py
@@ -3,7 +3,15 @@
 # WS server example
 
 import asyncio
+import json
+
 import websockets
+import logging
+
+from texasholdem import Deck
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 async def hello(websocket, path):
@@ -15,7 +23,20 @@ async def hello(websocket, path):
     await websocket.send(greeting)
     print(f"> {greeting}")
 
-start_server = websockets.serve(hello, "0.0.0.0", 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def random_hand(websocket, path):
+    async for message in websocket:
+        logger.debug("websocket: {}".format(websocket))
+        logger.debug("message: {}".format(message))
+        msg = json.loads(message)
+
+        ret = msg.message
+
+        await websocket.send(ret)
+
+
+if __name__ == '__main__':
+    start_server = websockets.serve(random_hand, "0.0.0.0", 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
