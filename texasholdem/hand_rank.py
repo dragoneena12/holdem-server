858de7af8cea660253dca75b4fc58df4e2662ca8
lapi <tyoutugai.7@gmail.com>
2021-01-04 15:31:42 +0900

    add hand_rank

--- texasholdem/hand_rank.py
+++ texasholdem/hand_rank.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 from typing import List
-from texasholdem import Card, Deck
+from texasholdem import Card
 
 import enum
 import logging
-
-from texasholdem.card import BASE_SUIT, BASE_NUMBERS
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -25,7 +23,7 @@ RANK_ORDER = [
 ]
 
 
-class RankName(enum.Enum):
+class RankName(enum.IntEnum):
     royal_flush = 10
     straight_flush = 9
     four_of_a_kind = 8
@@ -36,6 +34,45 @@ class RankName(enum.Enum):
     two_pair = 3
     one_pair = 2
     high_card = 1
+
+
+class HandRank:
+    def __init__(self, cards: List[Card] = None):
+        if len(cards) < 5:
+            logger.error("not enough cards to decide handrank")
+            return
+        else:
+            rank = None
+            n = len(cards)
+            for i in range(2 ** n):
+                picked_cards = []
+                for j in range(n):
+                    if (i >> j) & 1:
+                        picked_cards.append(cards[j])
+                if len(picked_cards) != 5:
+                    continue
+                if rank is None:
+                    rank = HandRank5(picked_cards)
+                else:
+                    rank = (
+                        HandRank5(picked_cards)
+                        if HandRank5(picked_cards) > rank
+                        else rank
+                    )
+                self.rank = rank
+
+    def __eq__(self, other: HandRank):
+        if not isinstance(other, HandRank):
+            return NotImplemented
+        return self.rank == other.rank
+
+    def __lt__(self, other: HandRank):
+        if not isinstance(other, HandRank):
+            return NotImplemented
+        return self.rank < other.rank
+
+    def __str__(self) -> str:
+        return str(self.rank)
 
 
 class HandRank5:
@@ -50,7 +87,7 @@ class HandRank5:
         if len(cards) != 5:
             logger.error("cannot call get_rank with other than 5 cards")
             raise Exception
-        number_count = [0 for c in range(14)]
+        number_count = [0 for c in range(15)]
         suit_count = {}
         isStraight = False
         isFlush = False
@@ -68,9 +105,9 @@ class HandRank5:
         rank_name = RankName.high_card
         # pair系hand判定
         for i in range(2, 15):
-            if number_count(i) == 1:
+            if number_count[i] == 1:
                 kicker.insert(0, i)
-            elif number_count(i) == 2:
+            elif number_count[i] == 2:
                 if rank_name == RankName.high_card:
                     rank_name = RankName.one_pair
                     rank_number.insert(0, i)
@@ -80,21 +117,21 @@ class HandRank5:
                 elif rank_name == RankName.three_of_a_kind:
                     rank_name = RankName.full_house
                     rank_number.append(i)
-            elif number_count(i) == 3:
+            elif number_count[i] == 3:
                 if rank_name == RankName.high_card:
                     rank_name = RankName.three_of_a_kind
                     rank_number.insert(0, i)
                 elif rank_name == RankName.one_pair:
                     rank_name = RankName.full_house
                     rank_number.insert(0, i)
-            elif number_count(i) == 4:
+            elif number_count[i] == 4:
                 rank_name = RankName.four_of_a_kind
                 rank_number.insert(0, i)
         # Straight判定
         for i in range(1, 11):
             flag = True
             for j in range(5):
-                if number_count(i + j) != 1:
+                if number_count[i + j] != 1:
                     flag = False
             if flag:
                 isStraight = True
@@ -111,9 +148,6 @@ class HandRank5:
             rank_name = RankName.flush
         return (rank_name, rank_number, kicker)
 
-    def to_dict(self):
-        return {"rank": self.rank, "kicker": self.kicker.to_dict()}
-
     def __eq__(self, other: HandRank5):
         if not isinstance(other, HandRank5):
             return NotImplemented
@@ -126,22 +160,28 @@ class HandRank5:
     def __lt__(self, other):
         if not isinstance(other, HandRank5):
             return NotImplemented
-        return self.rank_name < other.rank_name or (
-            self.rank_name == other.rank_name and self.kicker < other.kicker
-        )
+        if self.rank_name < other.rank_name:
+            return True
+        elif self.rank_name > other.rank_name:
+            return False
+        else:
+            for i in range(len(self.rank_number)):
+                if self.rank_number[i] > other.rank_number[i]:
+                    return False
+                elif self.rank_number[i] < other.rank_number[i]:
+                    return True
+            for i in range(len(self.kicker)):
+                if self.kicker[i] > other.kicker[i]:
+                    return False
+                elif self.kicker[i] < other.kicker[i]:
+                    return True
+            return False
 
     def __str__(self) -> str:
-        return str(self.rank)
+        return str(self.rank_name)
 
 
 if __name__ == "__main__":
-    logger.debug("BASE_SUIT: {}".format(BASE_SUIT))
-    logger.debug("BASE_NUMBERS: {}".format(BASE_NUMBERS))
-    deck = Deck()
-    logger.debug("Original Cards: {}".format(deck))
-    deck.shuffle()
-    logger.debug("Shuffled Cards: {}".format(deck))
-    draw = deck.draw(1)
-    logger.debug("Drawn: {}, Deck:{}".format(draw, deck))
-    draw = deck.draw(2)
-    logger.debug("Drawn: {}, Deck:{}".format(draw, deck))
+    cards = [Card(13, "H"), Card(13, "D"), Card(12, "H"), Card(12, "D"), Card(10, "S")]
+    rank = HandRank5(cards)
+    logger.debug("RankName:{}".format(rank))
